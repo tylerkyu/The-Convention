0b20a2f068aa33e91ec24174441a355e615f5a66
tylerkyu <146486350+tylerkyu@users.noreply.github.com>
2023-11-06 13:52:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let params = `width=300,height=300`;
+function ticpop() {
+    window.open('ticket.html', 'test', params);
+}
+function mempop() {
+    window.open('member.html', 'test', params);
+}
+function donpop() {
+    window.open('donate.html', 'test', params);
+}
+function noti() {
+    
+}
+
